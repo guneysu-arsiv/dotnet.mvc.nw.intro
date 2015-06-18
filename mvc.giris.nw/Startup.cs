c35c24949aa628de9a0cc0e6d62562565044822f
Ahmed Seref GUNEYSU <serefguneysu@gmail.com>
2015-06-18 19:04:42 +0300

    Bismillah

---
+++ mvc.giris.nw/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mvc.giris.nw.Startup))]
+namespace mvc.giris.nw
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
