a71a35b860a0a41f18448608543af1b1aeb024a8
Ahmed Seref GUNEYSU <serefguneysu@gmail.com>
2015-06-19 11:52:18 +0300

    Category Completed

--- mvc.northwind/Controllers/CategoryController.cs
+++ mvc.northwind/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Data.Entity;
+using System.Linq;
 using System.Web.Mvc;
 using mvc.northwind.EF;
 
@@ -16,13 +17,53 @@ namespace mvc.northwind.Controllers
             return View();
         }
 
+   
         public ActionResult Display(int id)
         {
+            // Yeni Category için buttonTitle ViewBag.ButtonTitle = "Kaydet" olacak.
             var category = db.Categories.FirstOrDefault(x => x.CategoryID == id);
+            ViewBag.ButtonTitle = "Update";
+            ViewBag.Title = "Category Display/Update";
+            ViewBag.Action = "/Category/Update";
             return View(category);
         }
 
-        // POST: Category/Update
+        public ActionResult Update(Category category)
+        {
+            /* 
+             * http://stackoverflow.com/q/15336248/1766716
+             * View bütün property'leri içermediği için
+             * View'de olmayan property'ler siliniyor.
+
+             * db.Categories.Attach(category);
+             * db.Entry(category).State = EntityState.Modified;
+             * db.SaveChanges();
+             */
+            var original = db.Categories.Find(category.CategoryID);
+
+            if (original != null)
+            {
+                original.CategoryName = category.CategoryName;
+                original.Description = category.Description;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Create(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult New()
+        {
+            ViewBag.Title = "New Category";
+            ViewBag.ButtonTitle = "Create";
+            ViewBag.Action = "/Category/Create";
+            return View();
+        }
 
     }
 }
